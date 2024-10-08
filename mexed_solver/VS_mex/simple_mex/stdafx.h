f47092965aa83f2f6b92b98ef29cb3c2df8f4c95
kabu <kabukunz@gmail.com>
2024-10-08 09:21:33 +0200

    changes to stdafx.h

--- mexed_solver/VS_mex/simple_mex/stdafx.h
+++ mexed_solver/VS_mex/simple_mex/stdafx.h
@@ -5,7 +5,7 @@
 #ifndef MEX_COMPILE_FLAG //to distungish between mex-matlab and VS compilations 
 #pragma once
 
-#include "targetver.h"
+// #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
