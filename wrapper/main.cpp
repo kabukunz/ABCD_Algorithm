7e5e9c34c8bb87c72029f1187fca45aa88c521a8
kabu <kabukunz@gmail.com>
2024-10-11 15:27:58 +0200

    a main inside

--- wrapper/main.cpp
+++ wrapper/main.cpp
@@ -20,6 +20,17 @@ int main(int argc, char *argv[])
     // if (!read_triangle_mesh(inputFile, V, F))
     //     cout << "failed to load mesh" << endl;
 
+    // ABCD_PN_demo.m
+
+    // %% 
+    // in_mesh     = './data/D1_00478_bff.obj';    % V,F,TC,FTC
+    // target_mesh = 'uv';             %initialized by texture UV coordinates
+    // options.free_bnd       = 1;     %boundary is free except the anchors
+    // options.fixed_vertices = [];    %unconstrained
+    // options.is_uv_mesh     = 1;     %surface mesh
+    // run ABCD_MexWrapperScript.m
+
+
     // 
     // do st
     // 
